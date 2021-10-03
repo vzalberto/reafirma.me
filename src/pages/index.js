23061557557011f3018e694a0a520cae0cb84fe2
Alberto <alberto@getclue.com>
2021-10-03 00:59:29 -0500

    2

--- src/pages/index.js
+++ src/pages/index.js
@@ -147,7 +147,7 @@ const IndexPage = () => {
         </span>
       </h1>
       <p style={paragraphStyles}>
-        Una vez, que no podía concentrarme, me grabé una meditación guiada para <code style={codeStyles}>programar</code>.
+        Una vez, que no podía concentrarme, grabé una meditación guiada para <code style={codeStyles}>programar</code>.
         <br />
 
         <iframe title="soundcloud" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1087739332&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
